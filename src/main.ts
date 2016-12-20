02b3dfd08d5f662201e716880449ae252785b31d
Joel George V <joel.gv3@gmail.com>
2016-12-20 20:43:14 +0530

    Adding more examples of operators

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,38 @@ import 'core-js/es7/reflect';
 // RxJS
 import { Observable } from 'rxjs/Rx';
 
-var debounceEventSource = Observable.fromEvent(document.querySelector('.debounce-input'), 'keypress');
-debounceEventSource
+let eventSource = Observable.fromEvent(document.querySelector('.input'), 'keypress');
+
+eventSource
+    .map(value => value['key'])
+    .subscribe(value => {
+        (<HTMLInputElement>document.querySelector('.normal-output')).textContent += value;
+    });
+
+eventSource
+    .map(value => value['key'])
     .distinctUntilChanged()
+    .subscribe(value => {
+        (<HTMLInputElement>document.querySelector('.distinct-output')).textContent += value;
+    });
+
+eventSource
+    .map(value => value['key'])
+    .delay(200)
+    .subscribe(value => {
+        (<HTMLInputElement>document.querySelector('.delay-output')).textContent += value;
+    });
+
+eventSource
+    .map(value => value['key'])
+    .throttleTime(200)
+    .subscribe(value => {
+        (<HTMLInputElement>document.querySelector('.throttle-output')).textContent += value;
+    });
+
+eventSource
+    .map(value => value['key'])
     .debounceTime(800)
-    .subscribe(() => {
-        (<HTMLInputElement>document.querySelector('.debounce-output')).textContent = (<HTMLInputElement>document.querySelector('.debounce-input')).value;
+    .subscribe(value => {
+        (<HTMLInputElement>document.querySelector('.debounce-output')).textContent += value;
     });
