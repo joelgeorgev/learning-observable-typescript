fd4840ce6865d03f919cf83c6a50f6db6992ca59
Joel George V <joel.gv3@gmail.com>
2018-08-12 19:42:29 +0530

    Purge all semicolons

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,51 +1,48 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var helpers = require('./helpers');
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+var helpers = require('./helpers')
 
 module.exports = {
-    entry: {
-        'app': './src/main.ts'
-    },
-
-    resolve: {
-        extensions: ['', '.ts', '.js']
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.ts$/,
-                loaders: ['awesome-typescript-loader']
-            },
-            {
-                test: /\.html$/,
-                loader: 'html'
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file?name=assets/[name].[hash].[ext]'
-            },
-            {
-                test: /\.css$/,
-                exclude: helpers.root('src', 'app'),
-                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-            },
-            {
-                test: /\.css$/,
-                include: helpers.root('src', 'app'),
-                loader: 'raw'
-            }
-        ]
-    },
-
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ['app']
-        }),
-
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
+  entry: {
+    'app': './src/main.ts'
+  },
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        exclude: helpers.root('src', 'app'),
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+      },
+      {
+        test: /\.css$/,
+        include: helpers.root('src', 'app'),
+        loader: 'raw'
+      }
     ]
-};
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+}
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,41 @@
-import 'core-js/es6';
-import 'core-js/es7/reflect';
+import 'core-js/es6'
+import 'core-js/es7/reflect'
 
 // RxJS
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx'
 
-let eventSource = Observable.fromEvent(document.querySelector('.input'), 'keypress');
-
-eventSource
-    .map(value => value['key'])
-    .subscribe(value => {
-        (<HTMLInputElement>document.querySelector('.normal-output')).textContent += value;
-    });
+let eventSource = Observable.fromEvent(document.querySelector('.input'), 'keypress')
 
 eventSource
-    .map(value => value['key'])
-    .distinctUntilChanged()
-    .subscribe(value => {
-        (<HTMLInputElement>document.querySelector('.distinct-output')).textContent += value;
-    });
+  .map(value => value['key'])
+  .subscribe(value => {
+    (<HTMLInputElement>document.querySelector('.normal-output')).textContent += value
+  })
 
 eventSource
-    .map(value => value['key'])
-    .delay(200)
-    .subscribe(value => {
-        (<HTMLInputElement>document.querySelector('.delay-output')).textContent += value;
-    });
+  .map(value => value['key'])
+  .distinctUntilChanged()
+  .subscribe(value => {
+    (<HTMLInputElement>document.querySelector('.distinct-output')).textContent += value
+  })
 
 eventSource
-    .map(value => value['key'])
-    .throttleTime(200)
-    .subscribe(value => {
-        (<HTMLInputElement>document.querySelector('.throttle-output')).textContent += value;
-    });
+  .map(value => value['key'])
+  .delay(200)
+  .subscribe(value => {
+    (<HTMLInputElement>document.querySelector('.delay-output')).textContent += value
+  })
 
 eventSource
-    .map(value => value['key'])
-    .debounceTime(800)
-    .subscribe(value => {
-        (<HTMLInputElement>document.querySelector('.debounce-output')).textContent += value;
-    });
+  .map(value => value['key'])
+  .throttleTime(200)
+  .subscribe(value => {
+    (<HTMLInputElement>document.querySelector('.throttle-output')).textContent += value
+  })
+
+eventSource
+  .map(value => value['key'])
+  .debounceTime(800)
+  .subscribe(value => {
+    (<HTMLInputElement>document.querySelector('.debounce-output')).textContent += value
+  })
